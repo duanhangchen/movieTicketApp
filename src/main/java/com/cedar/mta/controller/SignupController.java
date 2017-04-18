782f9fafda0b93a5d47b32d8ba3bd9b774f577df
Sanchay Agrawal <sanchay@Sanchays-MacBook-Pro.local>
2017-04-17 20:54:56 -0400

    Login with sign-up Working

--- src/main/java/com/cedar/mta/controller/SignupController.java
+++ src/main/java/com/cedar/mta/controller/SignupController.java
@@ -1,15 +1,13 @@
 package com.cedar.mta.controller;
 
+import java.security.NoSuchAlgorithmException;
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.portlet.ModelAndView;
-
 import com.cedar.mta.entity.User;
 import com.cedar.mta.service.UserService;
 
@@ -18,18 +16,20 @@ public class SignupController {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@RequestMapping("/sign-up")
 	public String showSignUpPage() {
 		return "sign-up";
 	}
 
 	@RequestMapping(value = "/sign-up", method = RequestMethod.POST)
-	public String handleSignUp(HttpSession session,ModelMap model, @RequestParam String firstName, @RequestParam String lastName,
-			@RequestParam String email, @RequestParam String password) {
+	public String handleSignUp(HttpSession session, ModelMap model, @RequestParam String firstName,
+			@RequestParam String lastName, @RequestParam String email, @RequestParam String password) throws NoSuchAlgorithmException {
 		User currentUser = userService.createNewUser(firstName, lastName, email, password);
-		session.setAttribute("user",currentUser);
-		//System.out.println(currentUser.getFirstName());
+		session.setAttribute("user", currentUser);
+		// System.out.println(currentUser.getFirstName());
 		return "redirect: ";
 	}
+
+	
 }
--- src/main/java/com/cedar/mta/service/UserService.java
+++ src/main/java/com/cedar/mta/service/UserService.java
@@ -1,12 +1,11 @@
 package com.cedar.mta.service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Calendar;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.cedar.mta.entity.Movie;
 import com.cedar.mta.entity.User;
 import com.cedar.mta.repository.UserRepository;
 
@@ -14,24 +13,53 @@ import com.cedar.mta.repository.UserRepository;
 public class UserService {
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
 
 	public User findUserByEmail(String email) {
 		return userRepository.findByEmail(email);
 	}
-	
-	public User createNewUser ( String firstName, String lastName, String email, String password) {
+
+	public Boolean validateUser(String email, String password) throws NoSuchAlgorithmException {
+		User user = findUserByEmail(email);
+		if (user != null){
+			String hashedPassword = hashPassword(password);
+			return user.getPword().equals(hashedPassword);
+		}
+		return false;
+	}
+
+	public User createNewUser(String firstName, String lastName, String email, String password)
+			throws NoSuchAlgorithmException {
 		User newUser = new User();
 		newUser.setFirstName(firstName);
 		newUser.setLastName(lastName);
 		newUser.setEmail(email);
 		java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
 		newUser.setCreationDate(date);
-		newUser.setPword(password);
+		String hashedPassword = hashPassword(password);
+		newUser.setPword(hashedPassword);
 		userRepository.save(newUser);
 		return newUser;
 	}
+
+	public String hashPassword(String password) throws NoSuchAlgorithmException {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(password.getBytes("UTF-8"));
+			StringBuffer hexString = new StringBuffer();
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if (hex.length() == 1)
+					hexString.append('0');
+				hexString.append(hex);
+			}
+			return hexString.toString();
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
 }
